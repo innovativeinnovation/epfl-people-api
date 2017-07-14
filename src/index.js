6eca21d142afd9868274315454b55c4af78356fa
William Belle <william.belle@gmail.com>
2017-07-14 08:33:41 +0200

    implement sciper search

---
+++ src/index.js
@@ -0,0 +1,29 @@
+/*
+ * (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2017.
+ * See the LICENSE file for more details.
+ */
+
+'use strict';
+
+var got = require('got');
+
+var SEARCH_URL = 'https://search.epfl.ch/json/ws_search.action';
+
+var search = function(q,locale) {
+  var params = '?q=' + q + '&request_locle=' + locale;
+  return got(SEARCH_URL + params);
+};
+
+exports.findBySciper = function(sciper, locale) {
+  if (sciper < 100000 && sciper > 999999) {
+    return Promise.reject(new TypeError('Expected a sciper'));
+  }
+
+  locale = locale || 'en';
+
+  return search(sciper, locale).then(function(response) {
+    return JSON.parse(response.body);
+  }).catch(function(error) {
+    return error;
+  });
+};
---
+++ test/test.js
@@ -0,0 +1,42 @@
+/*
+ * (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, VPSI, 2017.
+ * See the LICENSE file for more details.
+ */
+
+'use strict';
+
+var should = require('chai').should();
+var rewire = require('rewire');
+
+var epflPeopleApi = require('../src/index.js');
+
+describe('epfl-people-api module', function() {
+
+  it('should throw an exception with a bad sciper', function() {
+    return epflPeopleApi.findBySciper('xxx', 'en').then(function() {
+    }).catch(function(err) {
+      err.message.should.equal('Expected a sciper');
+    });
+  });
+
+  it('should fail with a wrong service url', function(done) {
+    var epflPeopleApiMock = rewire('../src/index.js');
+    epflPeopleApiMock.__set__('SEARCH_URL', 'foobar');
+    var result = epflPeopleApiMock.findBySciper(280556, 'en');
+    result.then(function(response) {
+      should.fail();
+      done();
+    }).catch(function(reason) {
+      done();
+    });
+  });
+
+  it('should find sciper 128871', function() {
+    return epflPeopleApi.findBySciper(128871, 'en').then(function(res) {
+      res[0].name.should.equal('Duratti');
+      res[0].firstname.should.equal('Lindo');
+      res[0].office.should.equal('INN 015');
+    });
+  });
+
+});
