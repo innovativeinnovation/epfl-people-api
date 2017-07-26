5d2d4dc8ad81ccbf7fbcf669468433427067ab29
William Belle <william.belle@gmail.com>
2017-07-26 19:52:54 +0200

    add search by email

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@
 
 'use strict';
 
-var got = require('got');
+var got       = require('got');
+var validator = require('validator');
 
 var SEARCH_URL = 'https://search.epfl.ch/json/ws_search.action';
 
@@ -28,6 +29,28 @@ exports.findBySciper = function(sciper, locale) {
       var data = JSON.parse(response.body);
       if (data.length === 0) {
         throw new TypeError('Sciper does not exist');
+      }
+      resolve(data[0]);
+    }).catch(function(err) {
+      reject(err);
+    });
+  });
+};
+
+exports.findByEmail = function(email, locale) {
+  if (!validator.isEmail(email)) {
+    return Promise.reject(new TypeError('Expected an email'));
+  }
+
+  locale = locale || 'en';
+
+  var url = buildsearchUrl(email, locale);
+
+  return new Promise(function(resolve, reject) {
+    got(url).then(function(response) {
+      var data = JSON.parse(response.body);
+      if (data.length === 0) {
+        throw new TypeError('Email does not exist');
       }
       resolve(data[0]);
     }).catch(function(err) {
