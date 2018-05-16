4d0b3f3749a8a7fe16bd9648f3e05e2f924b117e
William Belle <william.belle@gmail.com>
2018-05-16 20:24:13 +0200

    rewrite code

--- test/test-find.js
+++ test/test-find.js
@@ -3,41 +3,37 @@
  * See the LICENSE file for more details.
  */
 
-var should = require('chai').should();
-var rewire = require('rewire');
+const should = require('chai').should();
+const rewire = require('rewire');
 
-var epflPeopleApi = require('../src/index.js');
+const epflPeopleApi = require('../src/index.js');
 
-describe('epfl-people-api find', function () {
+describe('epfl-people-api find', () => {
   // Sadly...
-  it('shouldn\'t find Taylor Swift', function () {
+  it('shouldn\'t find Taylor Swift', () => {
     return epflPeopleApi.find(
       'Taylor Swift',
       'en'
-    ).then(function (res) {
-      res.length.should.equal(0);
-    });
+    ).then((res) => res.length.should.equal(0));
   });
 
-  it('should find Lindo Duratti', function () {
+  it('should find Lindo Duratti', () => {
     return epflPeopleApi.find(
       'Lindo Duratti',
       null
-    ).then(function (res) {
+    ).then((res) => {
       res.length.should.equal(1);
       res[0].sciper.should.equal('128871');
     });
   });
 
-  it('should fail with a wrong service url', function (done) {
-    var epflPeopleApiMock = rewire('../src/index.js');
+  it('should fail with a wrong service url', (done) => {
+    let epflPeopleApiMock = rewire('../src/index.js');
     epflPeopleApiMock.__set__('SEARCH_URL', 'foobar');
-    var result = epflPeopleApiMock.find('Lindo', 'en');
-    result.then(function (response) {
+    let result = epflPeopleApiMock.find('Lindo', 'en');
+    result.then((response) => {
       should.fail();
       done();
-    }).catch(function (reason) {
-      done();
-    });
+    }).catch((reason) => done());
   });
 });
--- test/test-sciper.js
+++ test/test-sciper.js
@@ -3,54 +3,44 @@
  * See the LICENSE file for more details.
  */
 
-var should = require('chai').should();
-var rewire = require('rewire');
+const should = require('chai').should();
+const rewire = require('rewire');
 
-var epflPeopleApi = require('../src/index.js');
+const epflPeopleApi = require('../src/index.js');
 
-describe('epfl-people-api findBySciper', function () {
-  it('should throw an exception with sciper xxx', function () {
-    return epflPeopleApi.findBySciper('xxx', 'en').then(function () {
-    }).catch(function (err) {
-      err.message.should.equal('Expected a sciper');
-    });
+describe('epfl-people-api findBySciper', () => {
+  it('should throw an exception with sciper xxx', () => {
+    return epflPeopleApi.findBySciper('xxx', 'en').then(() => {
+    }).catch((err) => err.message.should.equal('Expected a sciper'));
   });
 
-  it('should throw an exception with sciper 69', function () {
-    return epflPeopleApi.findBySciper(69, 'en').then(function () {
-    }).catch(function (err) {
-      err.message.should.equal('Expected a sciper');
-    });
+  it('should throw an exception with sciper 69', () => {
+    return epflPeopleApi.findBySciper(69).then(() => {
+    }).catch((err) => err.message.should.equal('Expected a sciper'));
   });
 
-  it('should throw an exception with sciper 1000051', function () {
-    return epflPeopleApi.findBySciper(1000051, 'en').then(function () {
-    }).catch(function (err) {
-      err.message.should.equal('Expected a sciper');
-    });
+  it('should throw an exception with sciper 1000051', () => {
+    return epflPeopleApi.findBySciper(1000051, 'en').then(() => {
+    }).catch((err) => err.message.should.equal('Expected a sciper'));
   });
 
-  it('should throw an exception with sciper 100000', function () {
-    return epflPeopleApi.findBySciper(100000, 'en').then(function () {
-    }).catch(function (err) {
-      err.message.should.equal('Sciper does not exist');
-    });
+  it('should throw an exception with sciper 100000', () => {
+    return epflPeopleApi.findBySciper(100000, 'en').then(() => {
+    }).catch((err) => err.message.should.equal('Sciper does not exist'));
   });
 
-  it('should fail with a wrong service url', function (done) {
-    var epflPeopleApiMock = rewire('../src/index.js');
+  it('should fail with a wrong service url', (done) => {
+    let epflPeopleApiMock = rewire('../src/index.js');
     epflPeopleApiMock.__set__('SEARCH_URL', 'foobar');
-    var result = epflPeopleApiMock.findBySciper(280556, 'en');
-    result.then(function (response) {
+    let result = epflPeopleApiMock.findBySciper(280556, 'en');
+    result.then((response) => {
       should.fail();
       done();
-    }).catch(function (reason) {
-      done();
-    });
+    }).catch((reason) => done());
   });
 
-  it('should find sciper 128871 in en', function () {
-    return epflPeopleApi.findBySciper(128871, 'en').then(function (res) {
+  it('should find sciper 128871 in en', () => {
+    return epflPeopleApi.findBySciper(128871, 'en').then((res) => {
       res.name.should.equal('Duratti');
       res.firstname.should.equal('Lindo');
       res.office.should.equal('INN 015');
@@ -58,14 +48,14 @@ describe('epfl-people-api findBySciper', function () {
     });
   });
 
-  it('should find sciper 128871 in fr', function () {
-    return epflPeopleApi.findBySciper(128871, 'fr').then(function (res) {
+  it('should find sciper 128871 in fr', () => {
+    return epflPeopleApi.findBySciper(128871, 'fr').then((res) => {
       res.position.should.equal('Informaticien');
     });
   });
 
   it('should find sciper 157489', function () {
-    return epflPeopleApi.findBySciper(157489, null).then(function (res) {
+    return epflPeopleApi.findBySciper(157489, undefined).then((res) => {
       res.name.should.equal('Boatto');
       res.firstname.should.equal('Laurent');
       res.office.should.equal('MA B0 519');
